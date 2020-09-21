9a587ea98084117c048bb01d1111d95ecabd62c6
Musnany <mustee3451@gmail.com>
2020-09-21 19:34:21 +0200

    innit

--- index.php
+++ index.php
@@ -174,7 +174,7 @@
 									<div class="tp-banner tp-banner0">
 										<ul>
 											<!-- SLIDE  -->
-											<li data-link="#" data-target="_self" data-transition="flyin" data-slotamount="7" data-masterspeed="500" data-saveperformance="on">
+											<li data-link="smartphones.html" data-target="_self" data-transition="flyin" data-slotamount="7" data-masterspeed="500" data-saveperformance="on">
 												<!-- MAIN IMAGE --><img src="img/dummy.png" alt="slidebg1" data-lazyload="img/photos/Background/bg.jpg" data-bgposition="left center" data-kenburns="off" data-duration="14000" data-ease="Linear.easeNone" data-bgpositionend="right center" />
 												<!-- LAYER NR. 1 -->
 												<div class="tp-caption very_big_white randomrotate customout rs-parallaxlevel-0" data-x="270" data-y="140" data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;" data-speed="300" data-start="500" data-end="4800" data-endspeed="300" data-easing="easeInOutBack" data-endeasing="easeOutBack" data-elementdelay="0.1" data-endelementdelay="0.1" style="z-index: 2;"> Trendy </div>
@@ -184,7 +184,7 @@
 												<div class="tp-caption large_white_text randomrotate customout rs-parallaxlevel-0" data-x="355" data-y="363" data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;" data-speed="300" data-start="1200" data-end="4800" data-endspeed="300" data-easing="easeInOutBack" data-endeasing="easeOutBack" data-elementdelay="0.1" data-endelementdelay="0.1" style="z-index: 2;"> SHOP NOW </div>
 
 											</li>
-											<li data-link="#" data-target="_self" data-transition="3dcurtain-horizontal" data-slotamount="7" data-masterspeed="500" data-saveperformance="on">
+											<li data-link="smartphones.html" data-target="_self" data-transition="3dcurtain-horizontal" data-slotamount="7" data-masterspeed="500" data-saveperformance="on">
 												<!-- MAIN IMAGE --><img src="img/dummy.png" alt="slidebg1" data-lazyload="img/photos/Phones/iphone-11-p.png" data-bgposition="left center" data-kenburns="off" data-duration="14000" data-ease="Linear.easeNone" data-bgpositionend="right center" />
 												<!-- LAYER NR. 1 -->
 												<div class="tp-caption very_big_white fade customout rs-parallaxlevel-0" data-x="270" data-y="140" data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.75;scaleY:0.75;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;" data-speed="300" data-start="500" data-end="4800" data-endspeed="300" data-easing="easeOutBack" data-endeasing="easeOutBack" data-elementdelay="0.1" data-endelementdelay="0.1" style="z-index: 2;"> Trendy </div>
@@ -677,126 +677,7 @@
 
 	<!-- Start Our Clients -->
 
-	<section id="Clients" class="light-wrapper">
-		<div class="container inner">
-			<div class="row">
-				<div class="Last-items-shop col-md-3 col-sm-6">
-
-					<!-- Block heading two -->
-					<div class="block-heading-two block-heading-three">
-						<h3><span>Special Offer</span></h3>
-					</div>
-					<!--<div class="Top-Title-SideBar">
-						<h3>Special Offer</h3>
-					</div>-->
-					<div class="Last-post">
-						<ul class="shop-res-items">
-							<li>
-								<a href="#"><img src="img/photos/Accessories/ear buds.jpg" alt=""></a>
-								<h6><a href="#">Ear Buds</a></h6>
-								<span class="sale-date">&#8358;1000.00</span>
-							</li>
-							<li>
-								<a href="#"><img src="img/photos/Accessories/Airpods.jpeg" alt=""></a>
-								<h6><a href="#">Airpods</a></h6>
-								<span class="sale-date">&#8358;1000.00</span>
-							</li>
-							<li>
-								<a href="#"><img src="img/photos/Accessories/ear buds.jpg" alt=""></a>
-								<h6><a href="#">Ear Buds</a></h6>
-								<span class="sale-date">&#8358;1000.00</span>
-							</li>
-						</ul>
-					</div>
-				</div>
-				<div class="Last-items-shop col-md-3 col-sm-6">
-					<!-- Block heading two -->
-					<div class="block-heading-two block-heading-three">
-						<h3><span>Best Sellers</span></h3>
-					</div>
-					<!--<div class="Top-Title-SideBar">
-						<h3>Best Sellers</h3>
-					</div>-->
-					<div class="Last-post">
-						<ul class="shop-res-items">
-							<li>
-								<a href="#"><img src="img/photos/Accessories/Airpods.jpeg" alt=""></a>
-								<h6><a href="#">Airpds</a></h6>
-								<span class="sale-date">&#8358;1000.00</span>
-							</li>
-							<li>
-								<a href="#"><img src="img/photos/Accessories/ear buds.jpg" alt=""></a>
-								<h6><a href="#">Ear Buds</a></h6>
-								<span class="sale-date">&#8358;1000.00</span>
-							</li>
-							<li>
-								<a href="#"><img src="img/photos/Accessories/Airpods.jpeg" alt=""></a>
-								<h6><a href="#">Airpds</a></h6>
-								<span class="sale-date">&#8358;1000.00</span>
-							</li>
-						</ul>
-					</div>
-				</div>
-				<div class="Last-items-shop col-md-3 col-sm-6">
-					<!-- Block heading two -->
-					<div class="block-heading-two block-heading-three">
-						<h3><span>Featured</span></h3>
-					</div>
-					<!--<div class="Top-Title-SideBar">
-						<h3>Featured</h3>
-					</div>-->
-					<div class="Last-post">
-						<ul class="shop-res-items">
-							<li>
-								<a href="#"><img src="img/photos/Accessories/ear buds.jpg" alt=""></a>
-								<h6><a href="#">Ear Buds</a></h6>
-								<span class="sale-date">&#8358;1000.00</span>
-							</li>
-							<li>
-								<a href="#"><img src="img/photos/Accessories/Airpods.jpeg" alt=""></a>
-								<h6><a href="#">Airpds</a></h6>
-								<span class="sale-date">&#8358;1000.00</span>
-							</li>
-							<li>
-								<a href="#"><img src="img/photos/Accessories/ear buds.jpg" alt=""></a>
-								<h6><a href="#">Ear Buds</a></h6>
-								<span class="sale-date">&#8358;1000.00</span>
-							</li>
-						</ul>
-					</div>
-				</div>
-				<div class="Last-items-shop col-md-3 col-sm-6">
-					<!-- Block heading two -->
-					<div class="block-heading-two block-heading-three">
-						<h3><span>Sales</span></h3>
-					</div>
-					<!--<div class="Top-Title-SideBar">
-						<h3>Sales</h3>
-					</div>-->
-					<div class="Last-post">
-						<ul class="shop-res-items">
-							<li>
-								<a href="#"><img src="img/photos/Accessories/Airpods.jpeg" alt=""></a>
-								<h6><a href="#">Airpods</a></h6>
-								<span class="sale-date">&#8358;1000.00</span>
-							</li>
-							<li>
-								<a href="#"><img src="img/photos/Accessories/ear buds.jpg" alt=""></a>
-								<h6><a href="#">Ear Buds</a></h6>
-								<span class="sale-date">&#8358;1000.00</span>
-							</li>
-							<li>
-								<a href="#"><img src="img/photos/Accessories/Airpods.jpeg" alt=""></a>
-								<h6><a href="#">Airpods</a></h6>
-								<span class="sale-date">&#8358;1000.00</span>
-							</li>
-						</ul>
-					</div>
-				</div>
-
-			</div>
-		</div>
-	</section>
+	
 
 	<!-- End Our Clients  -->
 
@@ -817,10 +698,11 @@
 							<a href="index.php"> <h1><i class="fa fa-shopping-cart color"></i> SwiftMart</h1></a>
 							<div class="footer-description" style="font-style: italic;">...The Amazon Of Africa</div>
 							<div class="footer-address"><i class="fa fa-map-marker color"></i> Nigeria<br> <i class="fa fa-phone color"></i> Phone: +2347017305233<br> <i class="fa fa-envelope color"></i> Email: <a href="mailto:info@swiftmart.com">info@swiftmart.com</a><br>
-								<a target="_blank" href="http://swiftmart.com/"><b>www.swiftmart.com</b></a>
+								<a target="_blank" href="https://swiftmart.com.ng/"><b>www.swiftmart.com.ng</b></a>
 							</div>
 							<div class="clear"></div>
 						</div>
+						
 						<div class="col-md-2 col-sm-4">
 							<h3 class="column-title">Customer Care</h3>
 							<ul class="column">
@@ -828,6 +710,7 @@
 								<li><a href="contact.html">Contact Us</a></li></ul>
 							<div class="clear"></div>
 						</div>
+						
 						
 						<div class="col-md-2 col-sm-4">
 							<h3 class="column-title">Shop Information</h3>
@@ -841,6 +724,12 @@
 						<div class="col-md-3">
 							
 							<div class="clear"></div>
+						</div>
+						<div class="input-group">
+							<input type="text" class="form-control" placeholder="Search...">
+									<span class="input-group-btn">
+										<button class="btn btn-color" id="srch" type="button"><i class="fa fa-search color"></i></button>
+									</span>
 						</div>
 					</div>
 				</div>
